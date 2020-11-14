1a9e1237e549936680a11039d5473a473f0701a4
Francois Lesage <48120561+storagebits@users.noreply.github.com>
2020-11-14 23:11:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,25 +11,18 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
     command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     if (command == "z") {
         RingbitCar.forward()
-        upleft = 0
     } else if (command == "s") {
         RingbitCar.back()
-        upleft = 0
     } else if (command == "w") {
         RingbitCar.brake()
     } else if (command == "q") {
         RingbitCar.turnleft()
-        upleft = 0
     } else if (command == "d") {
         RingbitCar.turnright()
-        upleft = 0
     } else if (command == "o") {
         blink = 1
     } else if (command == "p") {
-        blink = 0
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    } else if (command == "a") {
-        upleft = 1
     } else {
         RingbitCar.brake()
     }
@@ -39,7 +32,6 @@ input.onButtonPressed(Button.B, function () {
 })
 let command = ""
 let connected = 0
-let upleft = 0
 let blink = 0
 let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Ghost)
@@ -48,8 +40,8 @@ RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Green))
 blink = 0
-upleft = 0
 basic.forever(function () {
+    let upleft = 0
     if (blink == 1) {
         strip.showColor(neopixel.colors(NeoPixelColors.Green))
         basic.pause(100)
